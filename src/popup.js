2f7c4dd5b1d6ded06e44bb551b5fc71cd2034a66
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-08-16 12:50:22 +0300

    Add code comments

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,9 @@
-const idNumElement = document.getElementById("id_num");
+const idNumElement = document.getElementById("id_num"); // Getting a reference to the ID input element
 chrome.storage.local.get("id_value", (obj) => {
+	// If an ID text is already stored in the browser then it should appear on the extension popup
 	if (idNumElement) idNumElement.value = obj["id_value"] ? JSON.parse(obj["id_value"]) : "";
 });
 idNumElement.addEventListener("change", () => {
+	// Saving any modification to the ID text on the browser storage
 	chrome.storage.local.set({ "id_value": JSON.stringify(idNumElement.value) });
 });
